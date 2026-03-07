b12117b3478c6f314b14d535309522469d32c73d
Aforementioned1 <113794557+Aforementioned1@users.noreply.github.com>
2026-03-07 14:18:58 -0600

    Update at 'Sat Mar 07 14:18:58 CST 2026'

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -293,7 +293,7 @@ public class RobotContainer {
             Commands.runOnce(
                     () ->
                         drive.setPose(
-                            new Pose2d(drive.getPose().getTranslation(), Rotation2d.kZero)),
+                            new Pose2d(drive.getPose().getTranslation(), AllianceFlipUtil.apply(Rotation2d.kZero))),
                     drive)
                 .ignoringDisable(true));
 
